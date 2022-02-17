0e5e18d731d0d80a3015016841eb8ba3466def02
Jorge Berrizbeitia <58472035+jorgeberrizbeitia@users.noreply.github.com>
2022-02-17 16:49:25 +0100

    lecture done

--- index.js
+++ index.js
@@ -25,13 +25,35 @@ mongoose.connect("mongodb://localhost:27017/students-db")
   return StudentModel.insertMany(arrayOfStudents)
 })
 .then((response) => {
+  // response es los elementos añadidos
   console.log("agregando estudiantes")
 
   // busquedas 
+  // return StudentModel.find( { likesPokemon: true } ) // .todos los estudiantes ??
+  return StudentModel.find( { pizzaToppings: { $in: "Piña" } }, "name age", { limit: 1, sort:"name" } )
+  // los argumentos en orden: query, que propiedades quiero, todas las demas opciones.
+  // StudentModel.findOne => retorna un OBJ
 
 })
 
-// actualizar
+.then((response) => {
+
+  console.log(response)
+
+  // actualizar
+  return StudentModel.findOneAndUpdate( { name: "Blasco" }, { age: 23 }, { new: true } )
+  // primer argumento es el query y el segundo es que se quiere actualizar
+})
+.then((response) => {
+  // dolor de cabeza asi que anotar esto
+  // { new: true } hace que nos de la informacion actualizada
+  console.log("Blasco actualizado", response)
+  return StudentModel.findOneAndDelete( { name: "Eduard" } )
+})
+.then((response) => {
+  console.log("elemento borrado", response)
+})
+
 // borrar
 
 
